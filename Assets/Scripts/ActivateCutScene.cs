cbf689a68ef9881ba2fae997f1d24faf17533288
Tidlle <educarmo0707@gmail.com>
2024-10-07 13:33:32 -0300

    Cutscene

---
+++ Assets/Scripts/ActivateCutScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateCutScene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector playableDirector;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Cutscene Activated");
+            playableDirector.Play();
+            GetComponent<BoxCollider>().enabled = false;    
+        }
+    }
+}
