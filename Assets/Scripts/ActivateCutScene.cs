08fcf959f62983802318b65027ae9a18258e6a1b
vitinho <vitorfdiamantino@gmail.com>
2024-10-07 11:25:22 -0300

    Uptade jogo 3d

--- Assets/Scripts/ActivateCutScene.cs
+++ Assets/Scripts/ActivateCutScene.cs
@@ -7,16 +7,16 @@ public class ActivateCutScene : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private PlayableDirector playableDirector;
-    private void OntriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
             playableDirector.Play();
             GetComponent<BoxCollider>().enabled = false;
         }
-    {
         
     }
 
  
+
 
 }
